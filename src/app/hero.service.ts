8e4fb0f57fc4911f145ed17e31d4f8e76f7207b7
Tzu-Yi Li <alice1992224@gmail.com>
2017-05-08 20:37:24 +0800

    first commit - toh4 Service is done.

---
+++ src/app/hero.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+
+@Injectable()
+export class HeroService {
+	
+	// async
+	getHeroes(): Promise<Hero[]> {
+		return Promise.resolve(HEROES);
+	}
+	
+	// sync
+	/*
+	getHeroes(): Hero[] {
+		return HEROES;
+	}
+	*/
+
+	getHeroesSlowly(): Promise<Hero[]> {
+	  return new Promise(resolve => {
+	    // Simulate server latency with 2 second delay
+	    setTimeout(() => resolve(this.getHeroes()), 2000);
+	  });
+	}
+	
+	
+
+}
